5e2a42cea9dbf024e6e5c03abe68dbccebeced96
Guta101 <gustavo.tartaglia2@gmail.com>
2021-10-13 17:54:07 -0300

    Initial commit

---
+++ actions/actions.py
@@ -0,0 +1,29 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionProcurarArtigo(Action):
+
+    def name(self) -> Text:
+        return "action_procurar_artigo"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        materia = tracker.get_slot("materias")
+        texto = "Texto muito irado show de bola sobre a matéria"
+        dispatcher.utter_message("Aqui está o que eu achei sobre {}: {}".format(materia, texto))
+
+        return []
