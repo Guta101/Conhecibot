1876ab1077f4df8a5dc5ae544068e444acced96b
Guta101 <gustavo.tartaglia2@gmail.com>
2021-10-26 16:45:55 -0300

    Conexão com Postgre adicionada

--- actions/actions.py
+++ actions/actions.py
@@ -8,6 +8,7 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
+import psycopg2
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
@@ -23,7 +24,26 @@ class ActionProcurarArtigo(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         materia = tracker.get_slot("materias")
-        texto = "Texto muito irado show de bola sobre a matéria"
-        dispatcher.utter_message("Aqui está o que eu achei sobre {}: {}".format(materia, texto))
+        try:
+            connection = psycopg2.connect(user="admin",
+                                          password="password",
+                                          host="0.0.0.0",
+                                          port="5432",
+                                          database="postgres_db")
+            cursor = connection.cursor()
+            postgreSQL_select_Query = "select * from mobile"
+
+            cursor.execute(postgreSQL_select_Query)
+            print("Selecting rows from mobile table using cursor.fetchall")
+            mobile_records = cursor.fetchall()
+
+            print("Print each row and it's columns values")
+            for row in mobile_records:
+                print("Id = ", row[0], )
+                print("Model = ", row[1])
+                print("Price  = ", row[2], "\n")
+
+        except (Exception, psycopg2.Error) as error:
+            print("Erro ao procurar dados na tabela", error)
 
         return []
