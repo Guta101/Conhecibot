bf7e1c903d4d1a8dc4eb1adf9cfdee4ba9a64b6b
Guta101 <gustavo.tartaglia2@gmail.com>
2021-11-18 09:46:11 -0300

    Bugfix endpoints

--- actions/actions.py
+++ actions/actions.py
@@ -4,14 +4,10 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
-import psycopg2
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import json
 
 
 class ActionProcurarArtigo(Action):
@@ -23,27 +19,11 @@ class ActionProcurarArtigo(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        materia = tracker.get_slot("materias")
-        try:
-            connection = psycopg2.connect(user="admin",
-                                          password="password",
-                                          host="0.0.0.0",
-                                          port="5432",
-                                          database="postgres_db")
-            cursor = connection.cursor()
-            postgreSQL_select_Query = "select * from mobile"
+        materia = tracker.get_slot('materias')
+        #faz a busca na api do william que retorna um JSON
+        data = json.load(open('william.json'))
+        for artigo in data['artigos']:
+            titulo = artigo['title']
 
-            cursor.execute(postgreSQL_select_Query)
-            print("Selecting rows from mobile table using cursor.fetchall")
-            mobile_records = cursor.fetchall()
-
-            print("Print each row and it's columns values")
-            for row in mobile_records:
-                print("Id = ", row[0], )
-                print("Model = ", row[1])
-                print("Price  = ", row[2], "\n")
-
-        except (Exception, psycopg2.Error) as error:
-            print("Erro ao procurar dados na tabela", error)
 
         return []
